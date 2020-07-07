9374c62360dbe0b5aad129aceb0802646dc6be41
Ayush P Gupta <ayushpguptaapg@gmail.com>
2020-07-07 08:04:01 -0400

    Update basic home page

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
+    <v-app-bar id="app-bar" app color="white" elevation="0" height="64" clipped-left="true">
       <div class="d-flex align-center">
-        <p class="text-h5 font-weight-medium ma-0 pa-0">Sunserg</p>
+        <p class="text-h5 font-weight-light ma-0 pa-0">Sunserg</p>
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn href="https://www.sunserg.com/" target="_blank" text>
-        <span class="mr-2">WEBSITE</span>
+      <v-btn href="https://www.sunserg.com/" target="_blank" icon>
+        <v-icon>mdi-help-circle-outline</v-icon>
+      </v-btn>
+      <v-btn href="https://www.sunserg.com/" target="_blank" icon>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
+      <v-avatar size="32" class="ml-8 mr-1">
+        <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
+      </v-avatar>
     </v-app-bar>
 
     <v-content>
@@ -34,3 +39,11 @@ export default {
   })
 };
 </script>
+
+<style scoped>
+#app-bar {
+  border-width: 0 0 thin;
+  border-style: solid;
+  border-bottom-color: rgba(0, 0, 0, 0.12) !important;
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,83 @@
 <template>
   <v-container>
     <v-row class="text-center">
-      <v-col class="mb-4 mt-16">
-        <h1 class="display-2 font-weight-bold mb-3">Welcome to Sunserg</h1>
-
-        <p class="subheading font-weight-regular">
-          The Best Search and Organizing Tool for Your Company
-          <br />
-          <a href="https://www.sunserg.com/" target="_blank">Know more</a>
-        </p>
+      <v-col class="mb-4 mt-4">
+        <h1 class="display-2 font-weight-light mb-3">Welcome to Sunserg</h1>
+        <p
+          class="subheading font-weight-regular"
+        >The Best Search and Organizing Tool for Your Company</p>
       </v-col>
     </v-row>
+    <div class="text-h6 font-weight-regular text-center mb-2 px-4">Start by connecting your accounts</div>
+    <v-row class="justify-center">
+      <v-col cols="12" sm="6" md="4" lg="4" xl="3">
+        <v-card outlined>
+          <div class="d-flex flex-no-wrap justify-space-between align-center pa-4">
+            <div>
+              <v-card-title class="text-h6 font-weight-regular pa-0">Google Drive</v-card-title>
+              <v-card-text class="pa-0 pr-4">Easy and secure access to all of your content</v-card-text>
+            </div>
+            <v-spacer></v-spacer>
+            <v-img
+              src="https://cdn1.iconfinder.com/data/icons/logotypes/32/google-drive-512.png"
+              max-width="40"
+              contain
+            ></v-img>
+          </div>
+          <div class="d-flex justify-center my-2">
+            <v-btn text color="blue" class>+ Connect</v-btn>
+          </div>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6" md="4" lg="4" xl="3">
+        <v-card outlined>
+          <div class="d-flex flex-no-wrap justify-space-between align-center pa-4">
+            <div>
+              <v-card-title class="text-h6 pa-0 font-weight-regular">Slack</v-card-title>
+              <v-card-text class="pa-0 pr-4">Easy and secure access to all of your content</v-card-text>
+            </div>
+            <v-spacer></v-spacer>
+            <v-img
+              src="https://image.flaticon.com/icons/svg/2111/2111615.svg"
+              max-width="40"
+              contain
+            ></v-img>
+          </div>
+          <div class="d-flex justify-center my-2">
+            <v-btn text color="blue" class>+ Connect</v-btn>
+          </div>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6" md="4" lg="4" xl="3">
+        <v-card outlined>
+          <div class="d-flex flex-no-wrap justify-space-between align-center pa-4">
+            <div>
+              <v-card-title class="text-h6 pa-0 font-weight-regular">Gmail</v-card-title>
+              <v-card-text class="pa-0 pr-4">Gmail is email that's intuitive, efficient, and useful</v-card-text>
+            </div>
+            <v-spacer></v-spacer>
+            <v-img
+              src="https://image.flaticon.com/icons/png/512/281/281769.png"
+              max-width="40"
+              contain
+            ></v-img>
+          </div>
+          <div class="d-flex justify-center my-2">
+            <v-btn text color="blue" class>+ Connect</v-btn>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+    <div class="mt-16"></div>
+    <div class="text-center text-h4 font-weight-light">Stay connected on your device</div>
+    <p
+      class="text-center text-subheading font-weight-regular mt-2"
+    >The ease search of Sunserg, wherever you are.</p>
+    <v-img
+      src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
+      contain
+      height="64"
+    ></v-img>
   </v-container>
 </template>
 
