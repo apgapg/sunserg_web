1bd283f59c7996cd7c8f65a21f4617a513a4a796
Ayush P Gupta <ayushpguptaapg@gmail.com>
2020-07-20 13:04:40 +0530

    Improvise login page UI

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,7 +2,7 @@
   <v-container>
     <Intro />
     <div class="text-h6 font-weight-regular text-center mb-2 px-4">Sign in to your account</div>
-    <v-form v-model="valid">
+    <v-form ref="form" v-model="valid">
       <v-row class="justify-center my-2">
         <v-col cols="8" lg="3" sm="5">
           <div class="text-overline">EMAIL</div>
@@ -29,7 +29,15 @@
             append-icon="mdi-lock"
           ></v-text-field>
           <div class="text-center mt-2">
-            <v-btn to="/" :disabled="!valid" color="primary">SUBMIT</v-btn>
+            <v-btn :disabled="!valid" color="primary" @click="onLoginClick">
+              <v-icon left>mdi-key</v-icon>SUBMIT
+            </v-btn>
+          </div>
+          <div class="text-center mt-8">
+            <div>
+              Don't have an account?
+              <a href="/signup" class="text-decoration-underline">Sign Up</a>
+            </div>
           </div>
         </v-col>
       </v-row>
@@ -58,7 +66,15 @@ export default {
       v => !!v || "E-mail is required",
       v => /.+@.+/.test(v) || "E-mail must be valid"
     ]
-  })
+  }),
+  methods: {
+    onLoginClick: function() {
+      this.$refs.form.validate();
+      if (this.valid) {
+        this.$router.replace("/");
+      }
+    }
+  }
 };
 </script>
 
